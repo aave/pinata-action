386a40dfca971b34fd527f047c2e11f5aa67e99e
Sam Skynner <44842711+skynnes@users.noreply.github.com>
2024-08-01 09:36:33 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+#Adding Fake AWS Credentials to test security scanning
+AWS_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE
+AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
+main():
+  print("hello")
